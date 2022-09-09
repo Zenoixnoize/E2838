17db7a04c9d055439c7368c7de8494dc47ad94b4
ＺＥＮＯＩ <86784191+Zenoixnoize@users.noreply.github.com>
2022-09-09 09:46:42 +0530

    Update Config.js

--- Config.js
+++ Config.js
@@ -13,47 +13,47 @@ global.APIKeys = {
 	'https://zenzapis.xyz': 'sanzychan01', //credit goes to sanzy yt, if api key expire, buy it and put it here?\\
 }
 // 👈 You Can change this your choice
-global.emoji01 = ['🐼'] // 👈 Enter your Emoji 
-global.emoji02 = ['🦋'] //👈 Enter your Emoji
-global.notice = ` *Hi 🥰 I m*
-*ＮＯＩＺ Ξ*
-*I am 16 years old developer...*
+global.emoji01 = ['🎊'] // 👈 Enter your Emoji 
+global.emoji02 = ['🍂'] //👈 Enter your Emoji
+global.notice = ` *Hi 😾 I m*
+*Kasmira Lachintha*
+*I am 14 years old developer...*
 *From Sri Lanka 🇱🇰*
 *I am a Learning.*
 *Not more..*
 *Also I am Student*
 *Follow My Facebook - https://www.facebook.com/zenoixnoize*`  // 👈 Enter Your details and Social Media Links and Anything
-global.owner =  ['+94711502119'] //👈  Enter Your number
-global.premium =  ['+94711502119'] //👈  Enter Your number
-global.ownernomer = '+94711502119' //👈  Enter Your number
-global.ownername = 'ZENOI' //👈 Enter Your name
-global.botname = 'ＧＡＲＦＩＥＬＤ ＢＯＴ' //👈 Enter Your Bot name
+global.owner =  ['+94779391333'] //👈  Enter Your number
+global.premium =  ['+94779391333'] //👈  Enter Your number
+global.ownernomer = '+94779391333' //👈  Enter Your number
+global.ownername = 'Kasmira Lachintha' //👈 Enter Your name
+global.botname = '𝘾𝙃𝙀𝙀𝙈𝙀𝙎 𝘽𝙊𝙏 🍂' //👈 Enter Your Bot name
 global.footer = '© 𝖯𝗈𝗐𝖾𝗋𝖾𝖽 𝖡𝗒 𝖦𝖺𝗋𝖿𝗂𝖾𝗅𝖽 𝖡𝗈𝗍' //👈 Enter Your Name
 global.ig = 'https://github.com/Zenoixnoize/GARFIELD-WHATSAPP-BOT-v8' // 👈 You Can change this your choice
 global.region = 'India, South India, Kerala' // 👈 You Can change this your choice
 global.sc = 'https://github.com/Zenoixnoize/GARFIELD-WHATSAPP-BOT-v8'
-global.fbt = 'Follw Us ℹ️' // 👈 Enter (Subscribe/Follow/Like now)
+global.fbt = 'Follw Us ⭐' // 👈 Enter (Subscribe/Follow/Like now)
 global.myweb = 'https://www.facebook.com/garfieldbots/'// 👈 Enter your Social media link to follow now button
-global.welcome = '*Welcome To The Group 🦋*' // 👈 You Can change this your choice
-global.bye = '*Bye bye Mother fucker 🐼*' // 👈 You Can change this your choice
+global.welcome = '*සාදරයෙන් පිළිගන්නවා 😼*' // 👈 You Can change this your choice
+global.bye = '*බායි ආයි හිතුනොත් වරෙන් 😽*' // 👈 You Can change this your choice
 global.packname = 'GARFIELD-BOTv8'  // 👈 You Can change this your choice 
 global.author = 'NOIZE DEVELOPMENT' // 👈 You Can change this your choice 
 global.sessionName = 'session' // 👈 You Can change this your choice
 global.prefa = ['','!','.','🛰️','⚓','🍎'] // 👈 You Can change this your choice
 //Go to https://imgbb.com/, upload a photo of your choice and put its link here
-global.thumb =  'https://i.ibb.co/Jz0mMnC/Pics-Art-22-06-03-01-01-46-876.png' 
+global.thumb =  'https://i.ibb.co/nBj4wXS/d71bkcw-56eceb3a-c5cd-4a82-b6ee-52c75c9331f7.jpg' 
 global.spoty = 'https://i.postimg.cc/HW2YbN1b/Pics-Art-22-08-09-16-12-14-489.png'
 //Go to https://imgbb.com/, upload a photo of your choice and put its link here
 global.sp = '⭔'
 global.mess = {
-    success: 'Done 🦋', // 👈 You Can change this your choice
-    admin: 'This Feature Is Only For Admin!', // 👈 You Can change this your choice
-    botAdmin: 'Bot Must Be Admin First!', // 👈 You Can change this your choice
+    success: 'හරි 🦋', // 👈 You Can change this your choice
+    admin: '*උබට බෑ උබ ඇඩ්මින් නෙවෙයිනෙ 😹*', // 👈 You Can change this your choice
+    botAdmin: '*මට ඇඩ්මින් දියන් 😾*!', // 👈 You Can change this your choice
     owner: 'This Feature Is Only For Owner!', // 👈 You Can change this your choice
-    group: 'Feature Used Only For Groups!', // 👈 You Can change this your choice
+    group: '*මේක ගෘප් වලදි විතරයි පුලුවන් 😾*', // 👈 You Can change this your choice
     private: 'Features Used Only For Private Chat!', // 👈 You Can change this your choice
     bot: 'This Feature Is Only For Bot', // 👈 You Can change this your choice
-    wait: '```Please Wait...```', // 👈 You Can change this your choice
+    wait: '*ටිකක් ඉන්නකෝ 💬*', // 👈 You Can change this your choice
     error: 'Error! Maybe Api Key Is Expired🤔!', // 👈 You Can change this your choice
     endLimit: 'Your Daily Limit Has Expired, The Limit Will Be Reset Every 12 Hours',
 } // 👈 You Can change this your choice
